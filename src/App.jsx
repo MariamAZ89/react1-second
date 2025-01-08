3d1fac2695b772afccb8a49a2f27257529538750
MariamAZ89 <azami.maria89@gmail.com>
2025-01-08 17:37:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import MyNav from "./assets/Component/MyNav"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MyFooter from "./assets/Component/MyFooter"
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <MyNav/>
+    
+    <MyFooter/>
+    </>
+  )
+}
+
+export default App
